8cebccb25e11e6306c3e76911d78afd321bcd895
Yunwoo Jeong <jeongmallro99@gmail.com>
2024-11-11 16:45:36 +0900

    add @Repository to MemberRepository

--- src/main/java/com/example/salonreservation/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/salonreservation/domain/member/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.example.salonreservation.domain.member.repository;
 
 import com.example.salonreservation.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByKakaoMemberId(Long kakaoMemberId);
